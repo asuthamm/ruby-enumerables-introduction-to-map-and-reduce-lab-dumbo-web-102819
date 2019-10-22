0628713aaaca3672d8e60fee95a47d71d10cb90e
Alex Suthammanont <asuthamm@gmail.com>
2019-10-22 17:17:36 +0000

    Automatically backed up by Learn

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -31,7 +31,7 @@ describe 'my own reduce-like methods' do
   describe "reduce_to_total returns a running total when not given a starting point" do
     it "reduces correctly" do
       source_array = [1,2,3]
-      expect(reduce_to_total1(source_array)).to eq(6)
+      expect(reduce_to_total(source_array)).to eq(6)
     end
   end
 
@@ -39,7 +39,7 @@ describe 'my own reduce-like methods' do
     it "reduces correctly" do
       source_array = [1,2,3]
       starting_point = 100
-      expect(reduce_to_total2(source_array, starting_point)).to eq(106)
+      expect(reduce_to_total(source_array, starting_point)).to eq(106)
     end
   end
 
