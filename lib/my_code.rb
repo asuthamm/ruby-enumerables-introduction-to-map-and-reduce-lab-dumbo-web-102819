72a6b4d18b40e505f9af06bac77680a39a5e95a8
Alex Suthammanont <asuthamm@gmail.com>
2019-10-22 13:24:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,13 @@
 def map_to_negativize(arr)
-  arr.map { |ele| -ele }
+  arr_out = arr.map { |ele| -ele }
+  arr_out
 end
 
 def map_to_no_change(arr)
   arr_out = arr.map { |ele| ele }
+  arr_out
+end
+
+def map_to_double(arr)
   arr_out
 end
