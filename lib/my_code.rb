e23cf6e13eec8b2294aa80f049b65d94339fd5b3
Alex Suthammanont <asuthamm@gmail.com>
2019-10-22 16:56:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,12 +18,12 @@ def map_to_square(arr)
   arr_out
 end
 
-def reduce_like(arr)
+def reduce_to_total(arr)
   result = arr.reduce { |ele, sum| sum += ele  }
   result
 end
 
-# def reduce_to_total(arr,start)
-#   result = arr.reduce { |ele, sum| sum += ele  } + start
-#   # return result
-# end
+def reduce_to_total(arr,start)
+  result = arr.reduce { |ele, sum| sum += ele  } + start
+  # return result
+end
